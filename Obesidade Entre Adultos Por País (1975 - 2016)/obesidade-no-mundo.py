dea9a3f5a34118ef32ec56feacb277eeef50a535
Lucas Martins <lucasvm20@hotmail.com>
2023-09-26 00:19:03 -0300

    Quase Finalizado

--- Obesidade Entre Adultos Por País (1975 - 2016)/obesidade-no-mundo.py
+++ Obesidade Entre Adultos Por País (1975 - 2016)/obesidade-no-mundo.py
@@ -38,7 +38,7 @@ df.info()
 #################### Insights Sobre os Dados ####################
 
 
-# Percentual médio de obesidade por sexo no mundo em 2016
+# Percentual médio de obesidade por sexo no mundo em 2016 (último ano registrado)
 print('Feminino: ', end='')
 print('{:.3f}' .format(df[(df['Year'] == 2016) & (df['Sex'] == 'Female')]['Obesity'].mean()))
 
@@ -50,40 +50,42 @@ df[df['Year'] == 2016].groupby('Sex').mean()
 
 
 # %%
-# Os 5 países com a maior e a menor taxa de aumento nos índices de obesidade no período observado
-
-
+# Os 5 países com maior e a menor taxa de aumento nos índices de obesidade em todo período observado
 dif = df.groupby(['Country', 'Sex'])[['Obesity']].last() - df.groupby(['Country', 'Sex'])[['Obesity']].first()
 dif.reset_index(inplace=True)
 
-# Os 5 países com a maior taxa de aumento nos índices de obesidade no período observado
-print('\nOs 5 países com maior taxa de aumento nos índices de obesidade no período observado para ambos os sexos são:')
+# %%
+# Os 5 países com maior taxa de aumento nos índices de obesidade em todo período observado
+print('\nOs 5 países com maior taxa de aumento nos índices de obesidade em todo período observado para ambos os sexos são:')
 maiores = dif[dif['Sex'] == 'Both sexes'].nlargest(5, 'Obesity')
 print(maiores.reset_index())
 
-# Os 5 países com a menor taxa de aumento nos índices de obesidade no período observado
-print('\nOs 5 países com menor taxa de aumento nos índices de obesidade no período observado para ambos os sexos são:')
+# %%
+# Os 5 países com menor taxa de aumento nos índices de obesidade em todo período observado
+print('\nOs 5 países com menor taxa de aumento nos índices de obesidade em todo período observado para ambos os sexos são:')
 menores = dif[dif['Sex'] == 'Both sexes'].nsmallest(5, 'Obesity')
 print(menores.reset_index())
 
-# Quais os países com maiores e menores níveis percetuais de obesidade em 2015?
 
-print('\nPaíses com maiores e menores níveis percentuais de obesidade em 2015:')
+# %%
+# Países com maiores e menores níveis percentuais de obesidade em 2016
+
+print('\nPaíses com maiores e menores níveis percentuais de obesidade em 2016:')
 print('Maiores:')
-print(df[(df['Year'] == 2015) & (df['Sex'] == 'Both sexes')][['Country', 'Year', 'Sex', 'Obesity']].sort_values(['Obesity'], ascending=False).head(5).reset_index())
+print(df[(df['Year'] == 2016) & (df['Sex'] == 'Both sexes')][['Country', 'Year', 'Sex', 'Obesity']].sort_values(['Obesity'], ascending=False).head(5).reset_index())
 print('\nMenores:')
-print(df[(df['Year'] == 2015) & (df['Sex'] == 'Both sexes')][['Country', 'Year', 'Sex', 'Obesity']].sort_values(['Obesity']).head(5).reset_index())
+print(df[(df['Year'] == 2016) & (df['Sex'] == 'Both sexes')][['Country', 'Year', 'Sex', 'Obesity']].sort_values(['Obesity']).head(5).reset_index())
 
 '''
-df_2015 = df[df['Country'] == 2015]
-df_2015[df_2015['Obesity'] == df_2015['Obesity'].max()]
-df_2015[df_2015['Obesity'] == df_2015['Obesity'].min()]
+df_2016 = df[df['Country'] == 2016]
+df_2016[df_2016['Obesity'] == df_2016['Obesity'].max()]
+df_2016[df_2016['Obesity'] == df_2016['Obesity'].min()]
 '''
 
-
+# %%
 ##############################################################################################
 
-# Qual a diferença média percentual de obesidade entre sexos ao longo dos anos para o Brasil?
+# Diferença média percentual de obesidade entre sexos ao longo do período observado para o Brasil
 
 male = df[(df['Country'] == 'Brazil') & (df['Sex'] == 'Male')][['Year', 'Obesity']].set_index('Year')
 female = df[(df['Country'] == 'Brazil') & (df['Sex'] == 'Female')][['Year', 'Obesity']].set_index('Year')
@@ -98,14 +100,14 @@ df_brazil = df[df['Country'] == 'Brazil']
 df_brazil[df_brazil['Sex'] == 'Female']['Obesity'] - df_brazil[df_brazil['Sex'] == 'Male']['Obesity']
 '''
 
-
 ##############################################################################################
 
-# Você conseguiria plotar um gráfico mostrando a evolução da obesidade para ambos sexos no mundo?
+# %%
+# Gráfico com a evolução da obesidade para ambos sexos no mundo
 
 df_ambos = df[df['Sex'] == 'Both sexes'].groupby('Year').mean().reset_index()
 df_ambos.plot('Year', 'Obesity')
-df_ambos.boxplot('Year', 'Obesity')
+# df_ambos.boxplot('Year', 'Obesity')
 
 '''
 df_both = df[df['Sex'] == 'Both sexes']
@@ -113,3 +115,21 @@ df_both.groupby('Year')['Obesity'].mean().plot()
 '''
 
 # %%
+# Mapa com a evolução da obesidade para ambos os sexos no mundo
+import plotly.express as px
+
+df_mapa = df[df['Sex'] == 'Both sexes'].groupby(['Country', 'Year']).mean().reset_index()
+
+df_temp = px.data.gapminder()
+dict_iso_alpha = df_temp.set_index('country').to_dict()['iso_alpha']
+dict_num = {j: i for i, j in enumerate(df_mapa['Country'].unique())}
+
+df_mapa['iso_alpha'] = df_mapa['Country'].map(dict_iso_alpha)
+
+df_mapa['iso_num'] = df_mapa['Country'].map(dict_num)
+
+fig = px.choropleth(df_mapa.reset_index(drop=True), locations='iso_alpha', color='Obesity', hover_name='Country', animation_frame='Year')
+
+fig.update_layout(height=600)
+
+fig.show()
